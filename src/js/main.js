41a49e5b6684a36761b800bb74d740616c77b0b3
Oldook <sfgork@gmail.com>
2016-06-16 23:53:12 +0600

    cooooooookie

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ requirejs.config({
         marionette: '../../lib/backbone.marionette/lib/backbone.marionette',
         backbone: '../../lib/backbone/backbone',
         underscore: '../../lib/underscore/underscore',
+        cookie: '../../lib/js-cookie/src/js.cookie',
         bootstrap: '../../lib/bootstrap/dist/js/bootstrap',
         controller: 'app/controller',
         model: 'app/model',
